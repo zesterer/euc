ab7274e6797b503b34c464006af8578dcc42750b
martinsandfuchs <martin@cluginbuehl.ch>
2019-09-11 22:17:00 +0200

    Perspective correct interpolation for lines

--- src/rasterizer/lines.rs
+++ src/rasterizer/lines.rs
@@ -32,35 +32,39 @@ impl<'a, D: Target<Item=f32>> Rasterizer for Lines<'a, D> {
             .chunks_exact(2)
             .for_each(|verts| {
                 // Compute vertex shader outputs
-                let (a, a_vs_out) = pipeline.vert(&verts[0]);
-                let (b, b_vs_out) = pipeline.vert(&verts[1]);
+                let (a_hom, a_vs_out) = pipeline.vert(&verts[0]);
+                let (b_hom, b_vs_out) = pipeline.vert(&verts[1]);
 
-                // Compute homogenized coordinates
-                let a = Vec3::new(a[0], a[1], a[2]) / a[3];
-                let b = Vec3::new(b[0], b[1], b[2]) / b[3];
+                // Convert homogenous to euclidean coordinates
+                let a = Vec3::new(a_hom[0], a_hom[1], a_hom[2]) / a_hom[3];
+                let b = Vec3::new(b_hom[0], b_hom[1], b_hom[2]) / b_hom[3];
 
                 // Convert to framebuffer coordinates
                 let a_scr = half_scr * (Vec2::from(a) * MIRROR + 1.0);
                 let b_scr = half_scr * (Vec2::from(b) * MIRROR + 1.0);
 
+                let pa: Vec4<f32> = Vec4::from(a_hom);
+                let pb: Vec4<f32> = Vec4::from(b_hom);
+                let ab: Vec4<f32> = pb - pa;
+
                 let a_px = a_scr.map(|e| e as i32);
                 let b_px = b_scr.map(|e| e as i32);
-
+                
                 let min = Vec2::<i32>::min(a_px, b_px);
                 let max = Vec2::<i32>::max(a_px, b_px);
 
                 if (max.x - min.x) > (max.y - min.y) {
-                    let (l_scr, l_z, l_vs_out, r_scr, r_z, r_vs_out) =
+                    let (l_scr, r_scr) =
                         if a_scr.x < b_scr.x {
-                            (a_scr, a.z, a_vs_out, b_scr, b.z, b_vs_out)
+                            (a_scr, b_scr)
                         } else {
-                            (b_scr, b.z, b_vs_out, a_scr, a.z, a_vs_out)
+                            (b_scr, a_scr)
                         };
+                    let factor = 1.0 / (r_scr.x - l_scr.x);
+                    let m = Mat2::new(pa.w, -pa.x, -ab.w, ab.x) / (ab.x * pa.w - pa.x * ab.w);
 
-                    let dfrac = 1.0 / (r_scr.x - l_scr.x);
-                    let mut frac = 0.0;
                     for x in l_scr.x as i32..r_scr.x as i32 {
-                        let y = l_scr.y + frac * (r_scr.y - l_scr.y);
+                        let y = l_scr.y + (x as f32 - l_scr.x) * factor * (r_scr.y - l_scr.y);
 
                         // TODO: This is really bad bounds test code, fix this
                         if x < 0 || (x as usize) >= size.x || y < 0.0 || (y as usize) >= size.y {
@@ -68,7 +72,9 @@ impl<'a, D: Target<Item=f32>> Rasterizer for Lines<'a, D> {
                         }
 
                         // Calculate the interpolated depth of this fragment
-                        let z_lerped = f32::lerp2(l_z, r_z, 1.0 - frac, frac);
+                        let s_hom = m * Vec2::new(2.0 * x as f32 / size.x as f32 - 1.0, 1.0);
+                        let s = s_hom.x / s_hom.y;
+                        let z_lerped = (pa.z + s * ab.z) * s_hom.y;
 
                         let (x, y) = (x as usize, y as usize);
 
@@ -76,10 +82,10 @@ impl<'a, D: Target<Item=f32>> Rasterizer for Lines<'a, D> {
                         if z_lerped < unsafe { depth.get([x, y]) } {
                             // Calculate the interpolated vertex attributes of this fragment
                             let vs_out_lerped = P::VsOut::lerp2(
-                                l_vs_out.clone(),
-                                r_vs_out.clone(),
-                                1.0 - frac,
-                                frac,
+                                a_vs_out.clone(),
+                                b_vs_out.clone(),
+                                1.0 - s,
+                                s,
                             );
 
                             unsafe {
@@ -87,20 +93,20 @@ impl<'a, D: Target<Item=f32>> Rasterizer for Lines<'a, D> {
                                 target.set([x, y], pipeline.frag(&vs_out_lerped));
                             }
                         }
-                        frac += dfrac;
                     }
                 } else {
-                    let (l_scr, l_z, l_vs_out, r_scr, r_z, r_vs_out) =
+                    let (l_scr, r_scr) =
                         if a_scr.y < b_scr.y {
-                            (a_scr, a.z, a_vs_out, b_scr, b.z, b_vs_out)
+                            (a_scr, b_scr)
                         } else {
-                            (b_scr, b.z, b_vs_out, a_scr, a.z, a_vs_out)
+                            (b_scr, a_scr)
                         };
 
-                    let dfrac = 1.0 / (r_scr.y - l_scr.y);
-                    let mut frac = 0.0;
+                    let factor = 1.0 / (r_scr.y - l_scr.y);
+                    let m = Mat2::new(pa.w, -pa.y, -ab.w, ab.y) / (ab.y * pa.w - pa.y * ab.w);
+
                     for y in l_scr.y as i32..r_scr.y as i32 {
-                        let x = l_scr.x + frac * (r_scr.x - l_scr.x);
+                        let x = l_scr.x + (y as f32 - l_scr.y) * factor * (r_scr.x - l_scr.x);
 
                         // TODO: This is really bad bounds test code, fix this
                         if x < 0.0 || (x as usize) >= size.x || y < 0 || (y as usize) >= size.y {
@@ -108,7 +114,9 @@ impl<'a, D: Target<Item=f32>> Rasterizer for Lines<'a, D> {
                         }
 
                         // Calculate the interpolated depth of this fragment
-                        let z_lerped = f32::lerp2(l_z, r_z, 1.0 - frac, frac);
+                        let s_hom = m * Vec2::new(-2.0 * y as f32 / size.y as f32 + 1.0, 1.0);
+                        let s = s_hom.x / s_hom.y;
+                        let z_lerped = (pa.z + s * ab.z) * s_hom.y;
 
                         let (x, y) = (x as usize, y as usize);
 
@@ -116,10 +124,10 @@ impl<'a, D: Target<Item=f32>> Rasterizer for Lines<'a, D> {
                         if z_lerped < unsafe { depth.get([x, y]) } {
                             // Calculate the interpolated vertex attributes of this fragment
                             let vs_out_lerped = P::VsOut::lerp2(
-                                l_vs_out.clone(),
-                                r_vs_out.clone(),
-                                1.0 - frac,
-                                frac,
+                                a_vs_out.clone(),
+                                b_vs_out.clone(),
+                                1.0 - s,
+                                s,
                             );
 
                             unsafe {
@@ -127,7 +135,6 @@ impl<'a, D: Target<Item=f32>> Rasterizer for Lines<'a, D> {
                                 target.set([x, y], P::frag(pipeline, &vs_out_lerped));
                             }
                         }
-                        frac += dfrac;
                     }
                 }
             });
